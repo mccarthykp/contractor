f8c2abf1a1f58884e9f1d3f15b5206d78ae114b8
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-11-10 19:35:14 -0500

    added MongoDB and Donations model

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask.templating import render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Contractor
+donations = db.donations
 
 app = Flask(__name__)
 
-donations = [
-  { 'title': 'St. Jude', 'description': 'fund for underserved members of the community', 'amount': '1000' },
-  { 'title': 'Red Cross', 'description': 'fund to aid those affected by disasters', 'amount': '1000' }
-]
+# donations = [
+#   { 'title': 'St. Jude', 'description': 'fund for underserved members of the community', 'amount': '1000' },
+#   { 'title': 'Red Cross', 'description': 'fund to aid those affected by disasters', 'amount': '1000' }
+# ]
 
 # home/donations route ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 @app.route('/')
 def donations_index():
   ''' Show all donations '''
-  return render_template('donations_index.html', donations=donations)
+  return render_template('donations_index.html', donations=donations.find())
 
 
 
